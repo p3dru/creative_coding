9e11da0944d6306fe749460f9f1dc2339af70581
João Pedro <61830482+p3dru@users.noreply.github.com>
2022-12-21 12:56:31 -0300

    Add files via upload

--- Unidade 6 - Tipos de Esboço/sketch-07.js
+++ Unidade 6 - Tipos de Esboço/sketch-07.js
@@ -1,4 +1,165 @@
 const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
+
+const settings = {
+	dimensions: [ 1080, 1080 ]
+};
+
+let manager;
+
+let text = 'cn';
+let fontSize = 1200;
+let fontFamily = 'serif';
+
+const typeCanvas = document.createElement('canvas');
+const typeContext = typeCanvas.getContext('2d');
+
+const sketch = ({ context, width, height }) => {
+	const cell = 20;
+	const cols = Math.floor(width  / cell);
+	const rows = Math.floor(height / cell);
+	const numCells = cols * rows;
+
+	typeCanvas.width  = cols;
+	typeCanvas.height = rows;
+
+	return ({ context, width, height }) => {
+		typeContext.fillStyle = 'black';
+		typeContext.fillRect(0, 0, cols, rows);
+
+		fontSize = cols * 1.2;
+
+		typeContext.fillStyle = 'white';
+		typeContext.font = `${fontSize}px ${fontFamily}`;
+		typeContext.textBaseline = 'top';
+
+		const metrics = typeContext.measureText(text);
+		const mx = metrics.actualBoundingBoxLeft * -1;
+		const my = metrics.actualBoundingBoxAscent * -1;
+		const mw = metrics.actualBoundingBoxLeft + metrics.actualBoundingBoxRight;
+		const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
+
+		const tx = (cols - mw) * 0.5 - mx;
+		const ty = (rows - mh) * 0.5 - my;
+
+		typeContext.save();
+		typeContext.translate(tx, ty);
+
+		typeContext.beginPath();
+		typeContext.rect(mx, my, mw, mh);
+		typeContext.stroke();
+
+		typeContext.fillText(text, 0, 0);
+		typeContext.restore();
+
+		const typeData = typeContext.getImageData(0, 0, cols, rows).data;
+
+
+		context.fillStyle = 'black';
+		context.fillRect(0, 0, width, height);
+
+		context.textBaseline = 'middle';
+		context.textAlign = 'center';
+
+		// context.drawImage(typeCanvas, 0, 0);
+
+		for (let i = 0; i < numCells; i++) {
+			const col = i % cols;
+			const row = Math.floor(i / cols);
+
+			const x = col * cell;
+			const y = row * cell;
+
+			const r = typeData[i * 4 + 0];
+			const g = typeData[i * 4 + 1];
+			const b = typeData[i * 4 + 2];
+			const a = typeData[i * 4 + 3];
+
+			const glyph = getGlyph(r);
+
+			context.font = `${cell * 2}px ${fontFamily}`;
+			if (Math.random() < 0.1) context.font = `${cell * 6}px ${fontFamily}`;
+
+			context.fillStyle = 'white';
+
+			context.save();
+			context.translate(x, y);
+			context.translate(cell * 0.5, cell * 0.5);
+
+			// context.fillRect(0, 0, cell, cell);
+
+			context.fillText(glyph, 0, 0);
+			
+			context.restore();
+
+		}
+	};
+};
+
+const getGlyph = (v) => {
+	if (v < 50) return '';
+	if (v < 100) return '.';
+	if (v < 150) return '-';
+	if (v < 200) return '+';
+
+	const glyphs = 'csc, ad uhasd q'.split('');
+
+	return random.pick(glyphs);
+};
+
+
+const onKeyUp = (e) => {
+	text = e.key.toUpperCase();
+	manager.render();
+};
+
+document.addEventListener('keyup', onKeyUp);
+
+
+const start = async () => {
+	manager = await canvasSketch(sketch, settings);
+};
+
+start();
+
+
+
+
+
+/*
+const url = 'https://picsum.photos/200';
+
+const loadMeSomeImage = (url) => {
+	return new Promise((resolve, reject) => {
+		const img = new Image();
+		img.onload = () => resolve(img);
+		img.onerror = () => reject();
+		img.src = url;
+	});
+};
+
+const start = async () => {
+	const img = await loadMeSomeImage(url);
+	console.log('image width', img.width);
+	console.log('this line');
+};
+
+// const start = () => {
+// 	loadMeSomeImage(url).then(img => {
+// 		console.log('image width', img.width);
+// 	});
+// 	console.log('this line');
+// };
+
+
+start();
+*/
+
+
+
+/*
+const canvasSketch = require('canvas-sketch');
+const random = require('canvas-sketch-util/random');
 
 const settings = {
   dimensions: [ 1080, 1080 ],
@@ -11,14 +172,27 @@ let text = 'A';
 let fontSize = 1200;
 let fontFamily =  'Isidora Sans';
 
-const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'white';
-    context.fillRect(0, 0, width, height);
+const typeCanvas = document.createElement('canvas');
+const typeContext = typeCanvas.getContext('2d');
 
-    context.fillStyle =  'black';
-    context.font = `${fontSize}px ${fontFamily}`;
-    context.textBaseline = 'top';
+const sketch = ({context, width, height}) => {
+  const cell = 20;
+  const cols = Math.floor(width / cell);
+  const rows = Math.floor(height / cell);
+  const numCells = cols * rows;
+
+  typeCanvas.width = cols;
+  typeCanvas.height = rows;
+
+  return ({ context, width, height }) => {
+    typeContext.fillStyle = 'black';
+    typeContext.fillRect(0, 0, cols, rows);
+
+    fontSize = cols * 1.2;
+
+    typeContext.fillStyle =  'white';
+    typeContext.font = `${fontSize}px ${fontFamily}`;
+    typeContext.textBaseline = 'top';
 
     const metrics = context.measureText(text);             //mede o texto
     
@@ -29,20 +203,70 @@ const sketch = () => {
     const mh = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent;
 
     //centraliza as coordenadas x, y
-    const x = (width - mw) * 0.5 - mx;
-    const y = (width - mh) * 0.5 - my;
+    const tx = (cols - mw) * 0.5 - mx;
+    const ty = (rows - mh) * 0.5 - my;
 
     context.save();                           //salva o contexto atual
-    context.translate(x, y);
+    context.translate(tx, ty);
 
     context.beginPath();
     context.rect(mx, my, mw, mh);
     context.stroke();
     context.fillText(text, 0, 0);
     context.restore();                       //restaura o contexto
+
+    const typeData = typeContext.getImageData(0, 0, cols, rows).data;
+
+    context.fillStyle = 'black';
+    context.fillRect(0, 0, width, height);
+
+    context.textBaseline = 'middle';
+    context.textAlign = 'center';
+
+    for(let i = 0; i < numCells; i++){
+      const col = i % cols;
+      const row = Math.floor(i / cols);
+
+      const x = col * cell;
+      const y = row * cell;
+
+      const r = typeData[i * 4 + 0];
+      const g = typeData[i * 4 + 1];
+			const b = typeData[i * 4 + 2];
+			const a = typeData[i * 4 + 3];
+
+			const glyph = getGlyph(r);
+
+			context.font = `${cell * 2}px ${fontFamily}`;
+			if (Math.random() < 0.1) context.font = `${cell * 6}px ${fontFamily}`;
+
+			context.fillStyle = 'white';
+
+			context.save();
+			context.translate(x, y);
+			context.translate(cell * 0.5, cell * 0.5);
+
+			// context.fillRect(0, 0, cell, cell);
+
+			context.fillText(glyph, 0, 0);
+			
+			context.restore();
+
+
+    }
   };
 };
 
+const getGlyph = (v) => {
+	if (v < 50) return '';
+	if (v < 100) return '.';
+	if (v < 150) return '-';
+	if (v < 200) return '+';
+
+	const glyphs = '_= /'.split('');
+
+	return random.pick(glyphs);
+};
 
 //parametro do objeto para egar o texto toda vez que a tecça pressionada for solta
 const onKeyUp = (e) =>{
@@ -60,8 +284,10 @@ ciclo de execução normal, o síncrono). Ele faz um desvio na execução e carr
 o await informa que é necessário esperar a execução da função assíncrona, para realizar voltar
 o estado síncrono novamente.
 Envolvemos o canvas sketch em uma função async para podemos alterar o estado dela a cada nova 
-alteração percebida pela linha 54.
+alteração percebida pela linha document.addEventListener('keyup', onKeyUp);.
 */
+
+/*
 const start = async () => {
   manager = await canvasSketch(sketch, settings);
 };
@@ -69,7 +295,7 @@ const start = async () => {
 //inicia a função start na chamada da função que inicia o manager que 
 //é a função CanvasSketch em si e daí ai renderiza e inicia o desenho
 start();                            
-
+*/
 /*
 const url = 'https://picsum/photos/200';
 const loadMeSomeImage = (url) =>{
